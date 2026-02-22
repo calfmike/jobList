807f449e6a7568e9fb844032e1e6fb2663aac1b0
calfmike <juan.calfa@hotmail.com>
2026-02-22 18:50:44 -0300

    armandoEstados

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const baseURL =
+    "https://botfilter-h5ddh6dye8exb7ha.centralus-01.azurewebsites.net";
+  const [userInfo, setUserInfo] = useState({});
+  const [jobList, setJobList] = useState([]);
 
-  return <>
-  </>;
+  const getUserId = async () => {
+    const userId = await axios.get(
+      `${baseURL}/api/candidate/get-by-email?email=juan.calfa@hotmail.com`,
+    );
+    setUserInfo(userId.data);
+  };
+
+  const getJobList = async () => {
+    const jobList = await axios.get(`${baseURL}/api/jobs/get-list`);
+    setJobList(jobList.data);
+  };
+
+  useEffect(() => {
+    getUserId();
+    getJobList();
+  }, []);
+
+  return <></>;
 }
 
 export default App;
